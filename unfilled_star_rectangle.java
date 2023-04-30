6747ff6ab1b6de7644d3baa28bedc784b0686ad9
mehul-raj5 <125985871+mehul-raj5@users.noreply.github.com>
2023-05-01 04:08:57 +0530

    Update unfilled_star_rectangle.java

--- unfilled_star_rectangle.java
+++ unfilled_star_rectangle.java
@@ -1,7 +1,6 @@
 package src;
 public class unfilled_star_rectangle {
     public static void main(String[]args){
-    
         for(int i=1;i<=4;i++){
             if(i==1 || i==4){
                 for(int j=1;j<=4;j++){
